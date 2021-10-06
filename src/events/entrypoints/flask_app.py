3d24f3218d16c7849d614be15ddb0434cb5956c2
Jonatas Oliveira <contato@jonatasoliveira.me>
2021-10-06 10:50:38 -0300

    initial commit

---
+++ src/events/entrypoints/flask_app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/send_events", methods=["POST"])
+def send_events():
+    """Except unexpect value in the payload"""
+    """Except invalid timestamp"""
+    return "OK", 201
+
+
+@app.route("/search/}", methods=["GET"])
+def search():
+    """
+    params:
+        session
+        category
+        time range>>
+            time-start=2021-01-01 09:15:27.243860
+            time-end=2021-01-02 10:15:27.243860
+
+    """
+    return "OK", 200
