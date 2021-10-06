f6ee43f4ea8357ad83f7a1252e934d828ecd7b9b
Jonatas Oliveira <contato@jonatasoliveira.me>
2021-10-06 14:52:44 -0300

    add restx to use openapi

--- src/events/entrypoints/flask_app.py
+++ src/events/entrypoints/flask_app.py
@@ -1,24 +1,39 @@
 from flask import Flask
+from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
 
+api = Api(app, version='1.0', title='The EYE API',
+    description='Get events API',
+)
+ns = api.namespace('api', description='events endpoint')
 
-@app.route("/send_events", methods=["POST"])
-def send_events():
-    """Except unexpect value in the payload"""
-    """Except invalid timestamp"""
-    return "OK", 201
+@ns.route('/events')
+class Events(Resource):
+
+    def get(self, id):
+        return "OK", 200
+
+    def post(self, data):
+        return "OK", 201
 
 
-@app.route("/search/}", methods=["GET"])
-def search():
-    """
-    params:
-        session
-        category
-        time range>>
-            time-start=2021-01-01 09:15:27.243860
-            time-end=2021-01-02 10:15:27.243860
+# @app.route("/send_events", methods=["POST"])
+# def send_events():
+#     """Except unexpect value in the payload"""
+#     """Except invalid timestamp"""
+#     return "OK", 201
 
-    """
-    return "OK", 200
+
+# @app.route("/search/}", methods=["GET"])
+# def search():
+#     """
+#     params:
+#         session
+#         category
+#         time range>>
+#             time-start=2021-01-01 09:15:27.243860
+#             time-end=2021-01-02 10:15:27.243860
+
+#     """
+#     return "OK", 200
